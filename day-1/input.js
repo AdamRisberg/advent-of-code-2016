1fd97b7a4f897b5540ee1e6bf855a0d9ecb99127
Adam Risberg <myniztan@hotmail.com>
2020-01-24 14:52:00 -0600

    Add day 1 puzzle inputs

--- day-1/input.js
+++ day-1/input.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 const input = fs
   .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
-  .split("\r\n");
+  .split(", ")
+  .map(str => ({
+    turn: str[0],
+    walk: parseInt(str.slice(1))
+  }));
 
 module.exports = input;
