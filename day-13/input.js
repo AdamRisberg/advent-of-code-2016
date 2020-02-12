aaf2bfbf64cdc6b2bfb6c3101bd53455c92772b6
Adam Risberg <myniztan@hotmail.com>
2020-02-11 19:45:49 -0600

    Remove unneeded input files

--- day-13/input.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-
-const input = fs
-  .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
-  .split("\r\n");
-
-module.exports = input;
